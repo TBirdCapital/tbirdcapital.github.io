46aa0a9f7bd424e2279a11dbc5d576ef77ecd90f
enoughtaha <ahnaf@getcompound.com>
2020-08-04 14:42:32 -0400

    Updates

---
+++ precache-manifest.68563d82f9fb07dc9598c412e0bf934a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0f9412c45af1dd754a0a6ae4b19dfcc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "31e67632adb4bd89edba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0ff9e81da6fe239cfcdf",
+    "url": "/static/js/2.26a66939.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26a66939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31e67632adb4bd89edba",
+    "url": "/static/js/main.e086c1d2.chunk.js"
+  },
+  {
+    "revision": "6c9a4afb90cd75a0ad00",
+    "url": "/static/js/runtime-main.085de4b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
